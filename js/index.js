02c80a4d48cc402233c6248460c4f920caa700b3
WizkWong <chijianwong7@gmail.com>
2024-03-28 16:43:41 +0800

    seperate css file and design sign lanaguage website

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-document.getElementById('input-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const tpnumber = document.getElementById('tpnumber').value;
-    document.getElementById('text').textContent = name + " " + tpnumber;
+document.getElementById("input-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const tpnumber = document.getElementById("tpnumber").value;
+  
+  window.location.href = window.location.origin + "/sign-language.html"
 });
