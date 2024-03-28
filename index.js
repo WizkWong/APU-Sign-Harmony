ff5790b2e598f0f662a7242c8aebec087c4c8c35
WizkWong <chijianwong7@gmail.com>
2024-03-28 18:56:11 +0800

    add a-z alpahbets image and a feature - a new feature that receive the input, process the character of input, then show the alphabet image based on input.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 document.getElementById("student-input-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  const name = document.getElementById("name").value;
-  const tpnumber = document.getElementById("tpnumber").value;
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const tpnumber = document.getElementById("tpnumber").value;
 
-  const prerequisite = document.getElementById("prerequisite");
-  prerequisite.hidden = true;
+    const prerequisite = document.getElementById("prerequisite");
+    prerequisite.hidden = true;
 
-  const main = document.getElementById("main");
-  main.hidden = false;
-});
+    const main = document.getElementById("main");
+    main.hidden = false;
+  });
 
-document.getElementById('sign-language-input-form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const alphabet = document.getElementById('alphabet').value;
-  console.log(alphabet);
-  const sign_langauge = document.getElementById('sign-language-img');
-  sign_langauge.textContent = alphabet;
-});
+document.getElementById("sign-language-input-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const alphabet = document.getElementById("alphabet").value;
+
+    const charsList = alphabet.replace(/[^a-zA-Z]/g, '').split("");
+    
+    const imgHtml = charsList.reduce((html, c) => 
+      html.concat(`<div><img src="./assets/sign-language/${c}.jpg"/><p>${c.toUpperCase()}</p></div>\n`)
+    , "")
+
+    const sign_langauge = document.getElementById("sign-language-img");
+    sign_langauge.innerHTML = `<div class="flex-box">${imgHtml}</div>`;
+  });
