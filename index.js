406c7c02d4b48dc8a9cd370f5cc251e50d690e5c
WizkWong <chijianwong7@gmail.com>
2024-03-30 23:33:56 +0800

    ads border color change and add alert for form

--- index.js
+++ index.js
@@ -16,11 +16,7 @@ const generateSignLanguage = (alphabet) =>
               html.concat(`<div class="sign-box"><img src="${getImageUrl(c.toLowerCase())}"/><p>${c.toUpperCase()}</p></div>\n`)
             , "");
 
-document.getElementById("student-input-form").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const name = document.getElementById("name").value;
-    const tpnumber = document.getElementById("tpnumber").value;
-
+const insertDataToDB = async (name, tpnumber) => {
     const { error } = await supabase.from("student").insert({
       name: name,
       tp_number: tpnumber,
@@ -29,6 +25,19 @@ document.getElementById("student-input-form").addEventListener("submit", async (
     if (error) {
       console.log(error);
     }
+}
+
+document.getElementById("student-input-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const tpnumber = document.getElementById("tpnumber").value;
+
+    if (name.match(/[^a-zA-Z]/g)) {
+      alert('Only alphabet (A-Z) are allowed for the "Name" field!');
+      return;
+    }
+
+    insertDataToDB(name, tpnumber);
 
     const sign_langauge = document.getElementById("sign-language-img");
     
